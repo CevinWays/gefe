aff731f89a5699d25d7af8dc4f65ca0a78d0e621
cevin cevin <cevin@smltech.co.id>
2019-03-11 20:17:07 +0700

    adding laravel passport

--- resources/js/app.js
+++ resources/js/app.js
@@ -44,6 +44,7 @@ let routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue').default },
     { path: '/profile', component: require('./components/Profile.vue').default },
     { path: '/users', component: require('./components/Users.vue').default },
+    { path: '/developer', component: require('./components/Developer.vue').default },
     { path: '/vehicles', component: require('./components/Vehicles.vue').default }
 ]
 
@@ -80,6 +81,21 @@ window.Fire = Fire;
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue').default
+);
+
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue').default
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue').default
+);
 
 const app = new Vue({
     el: '#app',
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -106,6 +106,12 @@ to get the desired effect
             </router-link>
           </li>
           <li class="nav-item">
+            <router-link to="/developer" class="nav-link">
+              <i class="nav-icon fas fa-cogs"></i>
+              <p>Developer</p>
+            </router-link>
+          </li>
+          <li class="nav-item">
             <a class="nav-link" href="{{ route('logout') }}"
                 onclick="event.preventDefault();
                               document.getElementById('logout-form').submit();">
